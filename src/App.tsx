126a08054350dfc8e7212bdec53eec4f90d5da6d
Lipcimn <eduardoguicorrea@gmail.com>
2024-05-29 11:39:51 -0300

    change of class to hook (useState)

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,87 @@
-import { Button, TextField } from "@mui/material";
-import { system } from "./app/system";
+import {
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+  TextField,
+  Checkbox,
+} from "@mui/material";
+import Task from "./types/task";
 import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [name, setName] = useState("");
+  const [name, setName] = useState<string>("");
+  const [tasks, setTasks] = useState<Task[]>([]);
+  /**
+   * Handles the change event of the input element.
+   *
+   * @param {React.ChangeEvent<HTMLInputElement>} event - The change event.
+   * @return {void} This function does not return anything.
+   */
+  const handleInputValue = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    setName(event.target.value);
+  };
+  /**
+   * Handles the button click event.
+   *
+   * @return {void} This function does not return anything.
+   */
+  const handleButtonClick = (): void => {
+    if (name.trim() !== "") {
+      setTasks((tasks) => [...tasks, { name: name, completed: false }]);
+      setName("");
+    }
+  };
+  /**
+   * Updates the completion status of a task in the tasks array based on the checkbox event.
+   * @param {React.ChangeEvent<HTMLInputElement>} event - The event triggered by the checkbox change.
+   * @param {number} index - The index of the task in the tasks array.
+   * @return {void} This function does not return anything.
+   */
+  const handleTaskCheckbox = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ): void => {
+    const checked: boolean = event.target.checked;
+    const updatedTasks: Task[] = tasks.map((element, i) => {
+      return i === index ? { ...element, completed: checked } : element;
+    });
+    setTasks(updatedTasks);
+  };
   return (
     <>
-      <TextField
-        id="name-input"
-        label="Name"
-        variant="standard"
-        onChange={(element) => setName(element.target.value)}
-      />
-      <Button
-        variant="outlined"
-        onClick={() => {
-          system.newTask({ name: name, completed: false });
-          system.list.forEach((element) => console.log(element));
-        }}
-      >
-        Create
-      </Button>
+      <div>
+        <TextField
+          id="name-input"
+          label="Name"
+          variant="standard"
+          value={name}
+          onChange={handleInputValue}
+        />
+        <Button variant="outlined" onClick={handleButtonClick}>
+          Create
+        </Button>
+      </div>
+      <div>
+        <List>
+          {tasks.map((element, index) => {
+            return (
+              <ListItem key={index}>
+                <Checkbox
+                  onChange={(event) => handleTaskCheckbox(event, index)}
+                ></Checkbox>
+                <ListItemText
+                  primary={element.name}
+                  secondary={element.completed ? "Completed" : "Not Completed"}
+                />
+              </ListItem>
+            );
+          })}
+        </List>
+      </div>
     </>
   );
 }
---
+++ src/types/task.ts
@@ -0,0 +1,11 @@
+/**
+ * Definition of task props
+ * @prop {string} name - The name of the task
+ * @prop {boolean} completed - Set if the task is completed or not
+ */
+interface Task {
+  name: string;
+  completed: boolean;
+}
+
+export default Task
